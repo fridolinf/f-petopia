11263d342af0b4827a6b861f0858b3177e3afd60
fridolinf <frelly130@gmail.com>
2021-06-26 23:03:43 +0700

    Update VerifPayment.jsx

--- src/pages/Supplier/VerifPayment.jsx
+++ src/pages/Supplier/VerifPayment.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import { Result, Button } from 'antd';
 
 class VerifPayment extends Component {
 	render() {
-		return <div></div>;
+		return (
+			<div>
+				<Result status='success' title='Pembayaran Sukses!' />,
+			</div>
+		);
 	}
 }
 export default VerifPayment;
